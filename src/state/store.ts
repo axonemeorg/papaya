fad693bfec20dc74fcbbd111bdb0a3c773085c9e
Curtis Upshall <curtisupshall@gmail.com>
2022-04-04 09:35:39 -0700

    Database templating; added reducers to store.

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,11 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit'
 
 import categoriesReducer from '@/state/slices/categorySlice'
+import budgetsReducer from '@/state/slices/budgetSlice'
+import expendaturesReducer from '@/state/slices/expendatureSlice'
+import investmentsReducer from '@/state/slices/investmentSlice'
 
 const store = configureStore({
-    reducer: {
-		categories: categoriesReducer
-    },
+	reducer: {
+		budgets: budgetsReducer,
+		categories: categoriesReducer,
+		expendatures: expendaturesReducer,
+		investments: investmentsReducer
+	},
 })
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
