af930cedc742fe4e699c3c24cc70c45b56c3bdaa
Curtis Upshall <curtisupshall@gmail.com>
2024-09-17 18:11:27 -0700

    ZK-9: Updated db client to get SSL enable flag from .env file

--- web/src/database/client.ts
+++ web/src/database/client.ts
@@ -13,16 +13,20 @@ const user = process.env.DB_USER as string;
 const password = process.env.DB_PASSWORD as string;
 const port = Number(process.env.DB_PORT as string);
 const host = process.env.POSTGRES_DATABASE_HOST as string;
+const ssl = process.env.ENABLE_POSTGRES_SSL_MODE === 'true';
 
-const enableLogger = process.env.ENABLE_DB_LOGGER === 'true';
-
-export const pool = new Pool({
+const dbConfig = {
     host,
     database,
     user,
     password,
     port,
-});
+    ssl,
+};
+
+const enableLogger = process.env.ENABLE_DB_LOGGER === 'true';
+
+export const pool = new Pool(dbConfig);
 
 const db = drizzle(pool, { schema, logger: enableLogger });
 
