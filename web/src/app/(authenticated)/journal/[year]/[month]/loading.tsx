14da5b18ccd432c0a5a0ae3c21781115e54e1602
Curtis Upshall <curtisupshall@gmail.com>
2024-09-18 21:26:17 -0700

    ZK-12: Added placeholder loading component; Added calendar picker WIP

---
+++ web/src/app/(authenticated)/journal/[year]/[month]/loading.tsx
@@ -0,0 +1,5 @@
+export default function JournalLoading() {
+    return (
+        'Loading...'
+    )
+}
--- web/src/components/journal/JournalHeader.tsx
+++ web/src/components/journal/JournalHeader.tsx
@@ -1,9 +1,12 @@
 import { JournalDate } from "@/types/calendar";
 import { getJournalDateUrl, getNextYearMonth, getPreviousYearMonth, getTodayYearAndMonth } from "@/utils/Utils";
-import { Apps, ArrowBack, ArrowForward, EventRepeat, Today } from "@mui/icons-material";
-import { Button, ButtonBase, IconButton, Stack, Tooltip, Typography } from "@mui/material";
+import { Apps, ArrowBack, ArrowForward, ChevronLeft, ChevronRight, EventRepeat, Today } from "@mui/icons-material";
+import { Button, ButtonBase, IconButton, Popover, Stack, Tooltip, Typography } from "@mui/material";
+import { DateCalendar, LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import dayjs from "dayjs";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { PropsWithChildren } from "react";
 
 type JournalHeaderProps = PropsWithChildren<JournalDate>;
@@ -18,25 +21,47 @@ const formatMonthString = (year: number, month: number): string => {
 }
 
 export default function JournalHeader({ month, year, children }: JournalHeaderProps) {
+    const router = useRouter();
+
+    const handleChangeDatePickerDate = (value: dayjs.Dayjs) => {
+        const year = Number(value.format('YYYY'));
+        const month = Number(value.format('M'));
+
+        router.push(getJournalDateUrl({ year, month }));
+    }
+
     return (
         <Stack direction='row' justifyContent='space-between' sx={{ flex: 1 }} alignItems='center'>
             <Stack direction='row' alignItems='center' gap={2}>
                 <Stack direction='row' alignItems='center' gap={1}>
                     <Stack direction='row'>
                         <Link href={getJournalDateUrl(getPreviousYearMonth({ year, month }))}>
-                            <IconButton>
-                                <ArrowBack />
-                            </IconButton>
+                            <Tooltip title='Previous month'>
+                                <IconButton>
+                                    <ChevronLeft />
+                                </IconButton>
+                            </Tooltip>
                         </Link>
                         <Link href={getJournalDateUrl(getNextYearMonth({ year, month }))}>
-                            <IconButton>
-                                <ArrowForward />
-                            </IconButton>
+                            <Tooltip title='Next month'>
+                                <IconButton>
+                                    <ChevronRight />
+                                </IconButton>
+                            </Tooltip>
                         </Link>
                     </Stack>
                     <Typography variant='h5' sx={{ fontWeight: 500 }}>
                         {formatMonthString(year, month)}
                     </Typography>
+                    <Popover open={true}>
+                        
+                        <LocalizationProvider dateAdapter={AdapterDayjs}>
+                            <DateCalendar
+                                views={['month', 'year']}
+                                onChange={(value) => {handleChangeDatePickerDate(value)}}
+                            />
+                        </LocalizationProvider>
+                    </Popover>
                 </Stack>
                 <Link href={'/journal'}>
                     <Tooltip title='Today'>
