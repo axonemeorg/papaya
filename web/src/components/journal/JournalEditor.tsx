e2460a817fbdbe6961fc5470940d24272267a88f
Curtis Upshall <curtisupshall@gmail.com>
2024-09-19 11:23:09 -0700

    ZK-12: Refactored journal editor to remove table components and support small screens

--- web/src/components/journal/JournalEditor.tsx
+++ web/src/components/journal/JournalEditor.tsx
@@ -1,9 +1,9 @@
 'use client'
 
-import { MouseEvent, useEffect, useMemo, useState } from "react";
+import React, { MouseEvent, useEffect, useMemo, useState } from "react";
 import CreateJournalEntryModal from "../modal/CreateJournalEntryModal";
-import { alpha, Avatar, Box, Button, Chip, Fab, IconButton, List, ListItemIcon, ListItemText, MenuItem, Stack, Table, TableBody, TableCell, TableHead, TableRow, TextField, Typography } from "@mui/material";
-import { Add, Category as MuiCategoryIcon, Settings } from "@mui/icons-material";
+import { alpha, Avatar, Box, Button, ButtonBase, Chip, Fab, Grid2 as Grid, IconButton, List, ListItemIcon, ListItemText, MenuItem, Stack, Table, TableBody, TableCell, TableHead, TableRow, TextField, Typography, useMediaQuery, useTheme } from "@mui/material";
+import { Add, Category as MuiCategoryIcon } from "@mui/icons-material";
 import { Category, JournalEntry } from "@/types/get";
 import dayjs from "dayjs";
 import JournalEntryCard from "./JournalEntryCard";
@@ -19,22 +19,28 @@ import SettingsDrawer from "./categories/SettingsDrawer";
 import { useCategoryStore } from "@/store/useCategoriesStore";
 
 const JournalEntryDate = ({ day, isToday }: { day: dayjs.Dayjs, isToday: boolean })  => {
+    const theme = useTheme();
+    const isSmall = useMediaQuery(theme.breakpoints.down('sm'));
+
     return (
-        <Button color='inherit' sx={{ borderRadius: 1 }}>
-            <Stack direction='row' alignItems='flex-end' gap={0.75}>
-                <Avatar
-                    sx={(theme) => ({
-                        background: isToday ? theme.palette.primary.main : 'transparent',
-                        color: isToday ? theme.palette.primary.contrastText : 'inherit',
-                    })}
-                >
-                    {day.format('D')}
-                </Avatar>
-                <Typography mb={0.25} variant='overline' color={isToday ? 'primary' : undefined}>
-                    {day.format('MMM')},&nbsp;{day.format('ddd')}
-                </Typography>
-            </Stack>
-        </Button>
+        <Stack direction='row' alignItems='flex-end' gap={0.75} sx={{ p: 1, pb: isSmall ? 0 : undefined }}>
+            <Avatar
+                component={Button}
+                sx={(theme) => ({
+                    // display: 'block',
+                    background: isToday ? theme.palette.primary.main : 'transparent',
+                    color: isToday ? theme.palette.primary.contrastText : 'inherit',
+                    minWidth: 'unset',
+                    width: isSmall ? theme.spacing(4) : undefined,
+                    height: isSmall ? theme.spacing(4) : undefined,
+                })}
+            >
+                {day.format('D')}
+            </Avatar>
+            <Typography sx={{ mb: isSmall ? -0.25 : 0.25 }} variant='overline' color={isToday ? 'primary' : undefined}>
+                {day.format('MMM')},&nbsp;{day.format('ddd')}
+            </Typography>
+        </Stack>
     )
 }
 
@@ -53,6 +59,9 @@ export default function JournalEditor(props: JournalEditorProps) {
     
     const [selectedEntry, setSelectedEntry] = useState<JournalEntry | null>(null);
     const [selectedEntryAnchorEl, setSelectedEntryAnchorEl] = useState<HTMLElement | null>(null);
+
+    const theme = useTheme();
+    const isSmall = useMediaQuery(theme.breakpoints.down('sm'));
 
     const currentDayString = dayjs().format('YYYY-MM-DD');
 
@@ -102,13 +111,21 @@ export default function JournalEditor(props: JournalEditorProps) {
                     </JournalHeader>
                 }
                 user={props.user}
+                sx={{
+                    // width: '100dvw',
+                    // overflowX: 'auto',
+                    px: {
+                        sm: 0,
+                        md: 4,
+                    }
+                }}   
             >
                 <Fab
                     color='primary'
                     aria-label='add'
                     onClick={() => setShowJournalEntryModal(true)}
                     variant='extended'
-                    size='medium'
+                    size='large'
                     sx={(theme) => ({
                         position: 'fixed',
                         bottom: theme.spacing(4),
@@ -125,81 +142,92 @@ export default function JournalEditor(props: JournalEditorProps) {
                         anchorEl={selectedEntryAnchorEl}
                     />
                 )}
-                <Table size='small'>
-                    <TableBody>
-                        {Object
-                            .entries(journal)
-                            .sort(([dateA, _a], [dateB, _b]) => {
-                                return new Date(dateA).getTime() - new Date(dateB).getTime()
-                            })
-                            .map(([date, entries]) => {
-                                const day = dayjs(date);
-                                const isToday = day.isSame(dayjs(), 'day');
+                <Grid
+                    container
+                    columns={12}
+                    sx={{
+                        '--Grid-borderWidth': '1px',
+                        borderColor: 'divider',
+                        '& > div': {
+                            borderBottom: 'var(--Grid-borderWidth) solid',
+                            borderColor: 'divider',
 
-                                return (
-                                    <TableRow key={date}>
-                                        <TableCell width='180px' sx={{ verticalAlign: 'top' }}>
-                                            <JournalEntryDate day={day} isToday={isToday} />
-                                        </TableCell>
-                                        <TableCell sx={{ verticalAlign: 'top' }}>
-                                            {entries.length > 0 && (
-                                                <List>
-                                                    {entries.map((entry) => {
-                                                        const { category } = entry;
-                                                        const { netAmount } = entry;
-                                                        const isNetPositive = netAmount > 0;
+                            '&.date-cell': {
+                                borderWidth: {
+                                    xs: '0',
+                                    sm: '1px'
+                                }
+                            }
+                        },
+                    }}
+                >
+                    {Object
+                        .entries(journal)
+                        .sort(([dateA, _a], [dateB, _b]) => {
+                            return new Date(dateA).getTime() - new Date(dateB).getTime()
+                        })
+                        .map(([date, entries]) => {
+                            const day = dayjs(date);
+                            const isToday = day.isSame(dayjs(), 'day');
 
-                                                        return (
-                                                            <MenuItem
-                                                                key={entry.journalEntryId}
-                                                                sx={{ borderRadius: '64px' }}
-                                                                onClick={(event) => handleClickListItem(event, entry)}
-                                                            >
-                                                                <Stack direction='row' alignItems='center' gap={4}>
-                                                                    <Stack direction='row' alignItems='center'>
-                                                                        <ListItemIcon>
-                                                                            <CategoryIcon category={category} />
-                                                                        </ListItemIcon>
-                                                                        <ListItemText sx={{ width: 200 }}>{entry.memo}</ListItemText>
-                                                                    </Stack>
+                            return (
+                                <React.Fragment key={date}>
+                                    <Grid size={{ xs: 12, sm: 2 }} className='date-cell'>
+                                        <JournalEntryDate day={day} isToday={isToday} />
+                                    </Grid>
+                                    <Grid size={{ xs: 12, sm: 10 }}>
+                                        {entries.length > 0 && (
+                                            <List sx={{ pl: 1, pt: isSmall ? 0 : undefined }}>
+                                                {entries.map((entry) => {
+                                                    const { category } = entry;
+                                                    const { netAmount } = entry;
+                                                    const isNetPositive = netAmount > 0;
+
+                                                    return (
+                                                        <MenuItem
+                                                            key={entry.journalEntryId}
+                                                            sx={{ borderRadius: '64px' }}
+                                                            onClick={(event) => handleClickListItem(event, entry)}
+                                                        >
+                                                            <Grid container columns={12} sx={{ width: '100%', alignItems: 'center' }} spacing={2} rowSpacing={0}>
+                                                                <Grid size={{ xs: 12, sm: 4 }} sx={{ display: 'flex', flowFlow: 'row nowrap', }}>
+                                                                    <ListItemIcon sx={{ display: isSmall ? 'none' : undefined }}>
+                                                                        <CategoryIcon category={category} />
+                                                                    </ListItemIcon>
+                                                                    <ListItemText>{entry.memo}</ListItemText>
+                                                                </Grid>
+                                                                <Grid size={{ xs: 'auto', sm: 3, md: 2 }}>
                                                                     <ListItemText
-                                                                        sx={(theme) => ({ width: 100, textAlign: 'right', color: isNetPositive ? theme.palette.success.main : undefined })}
+                                                                        sx={(theme) => ({ textAlign: 'right', color: isNetPositive ? theme.palette.success.main : undefined })}
                                                                     >
                                                                         {getPriceString(netAmount)}
                                                                     </ListItemText>
-                                                                    <Box width={200}>
-                                                                        {category ? (
-                                                                            <CategoryChip category={category} />
-                                                                        ) : (
-                                                                            <Chip
-                                                                                sx={ (theme) => ({ backgroundColor: alpha(theme.palette.grey[400], 0.125) })}
-                                                                                label='Uncategorized'
-                                                                            />
-                                                                        )}
-                                                                    </Box>
-                                                                    {/* <Stack direction='row' sx={{ width: 300 }} gap={0.5}>
-                                                                        {methods.map((method) => {
-                                                                            return (
-                                                                                <Chip label={method.label} />
-                                                                            )
-                                                                        })}
-                                                                    </Stack> */}
-                                                                </Stack>
-                                                            </MenuItem>
-                                                        )
-                                                    })}
-                                                </List>
-                                            )}
-                                            {isToday && (
-                                                <QuickJournalEditor />
-                                            )}
-                                        </TableCell>
-                                    </TableRow>
-                                )
-                            })
-                        }
-                    </TableBody>
-                </Table>
+                                                                </Grid>
+                                                                <Grid size={{ xs: 'grow', sm: 5, md: 6 }}>
+                                                                    {category ? (
+                                                                        <CategoryChip category={category} />
+                                                                    ) : (
+                                                                        <Chip
+                                                                            sx={ (theme) => ({ backgroundColor: alpha(theme.palette.grey[400], 0.125) })}
+                                                                            label='Uncategorized'
+                                                                        />
+                                                                    )}
+                                                                </Grid>
+                                                            </Grid>
+                                                        </MenuItem>
+                                                    )
+                                                })}
+                                            </List>
+                                        )}
+                                        {isToday && (
+                                            <QuickJournalEditor />
+                                        )}
+                                    </Grid>
+                                </React.Fragment>
+                            )
+                        })
+                    }
+                </Grid>
             </BaseLayout>
         </NotificationsProvider>
     )
