2b19161ff340f464e09cf5a8aa90ef264b82243b
Curtis Upshall <curtisupshall@gmail.com>
2024-11-20 15:25:52 -0800

    ZK-29: Moved old drizzle migration, seeding and schema code to separate folder

--- web/src/database/client.ts
+++ web/src/database/client.ts
@@ -1,35 +1,4 @@
-import dotenv from 'dotenv';
-import dotenvExpand from 'dotenv-expand';
-import { drizzle } from 'drizzle-orm/node-postgres';
-import { Pool } from 'pg';
 
-import * as schema from './schemas'
+const POUCH_DB_NAME = process.env.POUCH_DB_NAME;
 
-const env = dotenv.config({ path: '../.env' });
-dotenvExpand.expand(env);
-
-const database = process.env.DB_NAME as string;
-const user = process.env.DB_USER as string;
-const password = process.env.DB_PASSWORD as string;
-const port = Number(process.env.DB_PORT as string);
-const host = process.env.POSTGRES_DATABASE_HOST as string;
-const ssl = process.env.ENABLE_POSTGRES_SSL_MODE === 'true';
-
-const dbConfig = {
-    host,
-    database,
-    user,
-    password,
-    port,
-    ssl,
-};
-
-const enableLogger = process.env.ENABLE_DB_LOGGER === 'true';
-
-export const pool = new Pool(dbConfig);
-
-const db = drizzle(pool, { schema, logger: enableLogger });
-
-export type DbClient = typeof db;
-
-export default db;
+export const db = new PouchDB(POUCH_DB_NAME);
