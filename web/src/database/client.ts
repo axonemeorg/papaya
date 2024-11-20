7bb67b31f01bf968ddcf4304a8d67b240009795b
Curtis Upshall <curtisupshall@gmail.com>
2024-11-20 15:55:23 -0800

    ZK-29: Redefined pouchdb database name

--- web/src/database/client.ts
+++ web/src/database/client.ts
@@ -1,7 +1,7 @@
 import PouchDB from 'pouchdb';
 import PouchDBFind from 'pouchdb-find';
 
-const POUCH_DB_NAME = process.env.POUCH_DB_NAME;
+const POUCH_DB_NAME = 'zisk-db';
 
 export const db = new PouchDB(POUCH_DB_NAME);
 
