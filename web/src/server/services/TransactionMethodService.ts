0f03481ae2b73e516cefdab53202aebfca5685c1
Curtis Upshall <curtisupshall@gmail.com>
2024-11-21 16:31:23 -0800

    ZK-29: Deleted old service and repo files

--- web/src/server/services/TransactionMethodService.ts
+++
@@ -1,7 +0,0 @@
-import TransactionMethodRepository from "../repositories/TransactionMethodRepository";
-
-export default class TransactionMethodService {
-    static async getUserTransactionMethods(userId: string) {
-        return TransactionMethodRepository.getUserTransactionMethods(userId);
-    }
-}
