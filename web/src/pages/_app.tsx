02d719455b86ebfb934b5b6dcb942fe1df2718bb
Curtis Upshall <curtisupshall@gmail.com>
2024-11-28 14:44:35 -0800

    ZK-38: Set up google authentication using next-auth

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -2,15 +2,18 @@ import { ThemeProvider } from '@mui/material'
 import { CssBaseline, Stack } from '@mui/material'
 import appTheme from '@/components/theme/theme'
 import { montserrat } from '@/fonts/montserrat'
-import '@/styles/main.scss'
 import Head from 'next/head'
+import { SessionProvider } from "next-auth/react"
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import NotificationsProvider from '@/providers/NotificationsProvider'
+
+import '@/styles/main.scss'
 
 const queryClient = new QueryClient();
 
 function MyApp(props: any) {
-  const { Component, pageProps } = props;
+  const { Component } = props;
+  const { session, ...rest } = props.pageProps;
 
   return (
     <>
@@ -21,11 +24,13 @@ function MyApp(props: any) {
       <ThemeProvider theme={appTheme}>
         <CssBaseline />
         <Stack component='main' id='root' minHeight='100dvh' className={montserrat.className}>
-          <QueryClientProvider client={queryClient}>
-            <NotificationsProvider>
-              <Component {...pageProps} />
-            </NotificationsProvider>
-          </QueryClientProvider>
+          <SessionProvider session={session}>
+            <QueryClientProvider client={queryClient}>
+              <NotificationsProvider>
+                <Component {...rest} />
+              </NotificationsProvider>
+            </QueryClientProvider>
+          </SessionProvider>
         </Stack>
       </ThemeProvider>
     </>
