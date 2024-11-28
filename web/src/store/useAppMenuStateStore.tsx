97a419f7d2a60c1cfd6b9d456b4192e8273ee157
Curtis Upshall <curtisupshall@gmail.com>
2024-11-27 16:00:45 -0800

    ZK-36: Use localStorage to cache menu open state (WIP)

--- web/src/store/useAppMenuStateStore.tsx
+++ web/src/store/useAppMenuStateStore.tsx
@@ -1,6 +1,5 @@
 import { create } from "zustand";
 
-
 interface AppMenuState {
     isOpen: boolean;
     toggleOpen: () => void;
@@ -8,9 +7,39 @@ interface AppMenuState {
     close: () => void;
 }
 
+const LOCAL_STORAGE_KEY = "ZISK_APP_MENU_OPEN_STATE";
+const DEFAULT_OPEN_STATE = true;
+
+const getInitialOpenState = (): boolean => {
+    if (typeof window !== "undefined") {
+        const storedState = localStorage.getItem(LOCAL_STORAGE_KEY);
+        return storedState === "true";
+    }
+    return DEFAULT_OPEN_STATE;
+};
+
 export const useAppMenuStateStore = create<AppMenuState>((set) => ({
-    isOpen: false,
-    toggleOpen: () => set((state) => ({ isOpen: !state.isOpen })),
-    open: () => set({ isOpen: true }),
-    close: () => set({ isOpen: false }),
+    isOpen: getInitialOpenState(),
+    toggleOpen: () =>
+        set((state) => {
+            const newState = !state.isOpen;
+            if (typeof window !== "undefined") {
+                localStorage.setItem(LOCAL_STORAGE_KEY, newState.toString());
+            }
+            return { isOpen: newState };
+        }),
+    open: () =>
+        set(() => {
+            if (typeof window !== "undefined") {
+                localStorage.setItem(LOCAL_STORAGE_KEY, "true");
+            }
+            return { isOpen: true };
+        }),
+    close: () =>
+        set(() => {
+            if (typeof window !== "undefined") {
+                localStorage.setItem(LOCAL_STORAGE_KEY, "false");
+            }
+            return { isOpen: false };
+        }),
 }));
