168a1701f83e3e0cd40e3c4d44acb25f779bdc66
Curtis Upshall <curtisupshall@gmail.com>
2024-11-28 21:01:18 -0800

    ZK-36: Created JournalSelect, SearchWidget, and SyncStatus components (not yet functional)

--- web/src/components/nav/header/Header.tsx
+++ web/src/components/nav/header/Header.tsx
@@ -1,9 +1,13 @@
 'use client';
 
-import { IconButton, Stack, useMediaQuery, useTheme } from "@mui/material";
+import { Avatar, Button, Chip, IconButton, Stack, Typography, useMediaQuery, useTheme } from "@mui/material";
 import AppLogo from "./AppLogo";
-import { Menu } from "@mui/icons-material";
+import { Menu, Search, Settings } from "@mui/icons-material";
 import { useAppMenuStateStore } from "@/store/useAppMenuStateStore";
+import { PropsWithChildren } from "react";
+import JournalSelect from "./JournalSelect";
+import SearchWidget from "./SearchWidget";
+import SyncStatus from "./SyncStatus";
 
 export default function Header() {
     const theme = useTheme();
@@ -14,9 +18,11 @@ export default function Header() {
 
     return (
         <Stack
+            component='header'
             direction='row'
             gap={1}
             alignItems='center'
+            justifyContent={'space-between'}
             sx={{
                 py: 1,
                 px: 1.5,
@@ -24,12 +30,49 @@ export default function Header() {
                 textDecoration: 'none',
             }}
         >
-            <IconButton onClick={() => toggleOpen()} size="large">
-                <Menu />
-            </IconButton>
-            {showLogo && (
-                <AppLogo />
-            )}
+            <Stack direction='row' gap={1} alignItems={'center'}>
+                <IconButton onClick={() => toggleOpen()} size="large">
+                    <Menu />
+                </IconButton>
+                {showLogo && (
+                    <AppLogo />
+                )}
+                <Stack direction='row' alignItems='center' gap={1} ml={2}>
+                    <JournalSelect />
+                    <SyncStatus />
+                </Stack>
+            </Stack>
+            <Stack direction='row' gap={1} alignItems={'center'}>
+                <SearchWidget />
+                <IconButton sx={(theme) => ({ color: theme.palette.text.secondary })}>
+                    <Settings />
+                </IconButton>
+                <Avatar />
+            </Stack>
         </Stack>
     )    
 }
+
+// const Shortcut = (props: PropsWithChildren) => {
+//     return (
+//         <Typography component='kbd' sx={(theme) => {
+//             return {
+//                 // all: 'unset',
+//                 // fontSize: theme.typography.pxToRem(12),
+//                 fontWeight: 'bold',
+//                 lineHeight: '19px',
+//                 marginLeft: theme.spacing(0.5),
+//                 border: `1px solid ${theme.palette.grey[200]}`,
+//                 backgroundColor: theme.palette.background.paper,
+//                 padding: theme.spacing(0, 0.5),
+//                 borderRadius: 7,
+//                 // ...theme.applyDarkStyles({
+//                 //     borderColor: (theme).palette.primaryDark[600],
+//                 //     backgroundColor: (theme).palette.primaryDark[800],
+//                 // }),
+//             };
+//         }}>
+//             {props.children}
+//         </Typography>
+//     )
+// }
---
+++ web/src/components/nav/header/SyncStatus.tsx
@@ -0,0 +1,137 @@
+'use client';
+
+import { CloudDone, CloudOff, CloudSync, Computer, Insights, ReceiptLong, Sync, SyncProblem, UnfoldMore } from "@mui/icons-material";
+import { Button, CircularProgress, Collapse, IconProps, ListItemText, SvgIconOwnProps, Typography } from "@mui/material";
+import { useEffect, useMemo, useState } from "react";
+
+enum SyncStatusEnum {
+    SAVING = 'SAVING',
+    SAVED_TO_REMOTE = 'SAVED_TO_REMOTE',
+    WORKING_OFFLINE = 'WORKING_OFFLINE',
+    WORKING_LOCALLY = 'WORKING_LOCALLY',
+    SAVED_TO_THIS_DEVICE = 'SAVED_TO_THIS_DEVICE',
+    FAILED_TO_SAVE = 'FAILED_TO_SAVE',
+    IDLE = 'IDLE'
+}
+
+type IconColor = SvgIconOwnProps['color'];
+
+export default function SyncStatus() {
+    const [verbose, setVerbose] = useState<boolean>(false);
+    const [syncStatus, setSyncStatus] = useState<SyncStatusEnum>(SyncStatusEnum.SAVING);
+
+    const syncStatusText = useMemo(() => {
+        switch (syncStatus) {
+            case SyncStatusEnum.SAVING:
+                return 'Syncing...';
+            case SyncStatusEnum.SAVED_TO_REMOTE:
+                return 'Saved to remote';
+            case SyncStatusEnum.WORKING_OFFLINE:
+                return 'Working offline';
+            case SyncStatusEnum.SAVED_TO_THIS_DEVICE:
+                return 'Saved to this device';
+            case SyncStatusEnum.WORKING_LOCALLY:
+                return 'Working locally';
+            case SyncStatusEnum.FAILED_TO_SAVE:
+                return 'Failed to save';
+            case SyncStatusEnum.IDLE:
+                return 'Idle';
+        }
+    }, []);
+
+    const syncIconVerboseColor: IconColor = useMemo(() => {
+        switch (syncStatus) {
+            case SyncStatusEnum.SAVED_TO_REMOTE:
+                return 'success';
+            case SyncStatusEnum.WORKING_OFFLINE:
+                return 'warning';
+            case SyncStatusEnum.FAILED_TO_SAVE:
+                return 'error';
+            case SyncStatusEnum.SAVED_TO_THIS_DEVICE:
+            case SyncStatusEnum.WORKING_LOCALLY:
+            case SyncStatusEnum.IDLE:
+            case SyncStatusEnum.SAVING:
+            default:
+                return undefined;
+        }
+    }, [syncStatus]);
+
+    const ButtonIcon = useMemo(() => {
+
+        const iconColor = syncIconVerboseColor ?? 'inherit';
+
+        switch (syncStatus) {
+            case SyncStatusEnum.SAVING:
+                return (
+                    <Sync color={iconColor} />
+                )
+            case SyncStatusEnum.SAVED_TO_REMOTE:
+                return (
+                    <CloudDone color={iconColor} />
+                )
+            case SyncStatusEnum.WORKING_OFFLINE:
+                return (
+                    <CloudOff color={iconColor} />
+                )
+            case SyncStatusEnum.WORKING_LOCALLY:
+            case SyncStatusEnum.SAVED_TO_THIS_DEVICE:
+                return (
+                    <Computer color={iconColor} />
+                )
+            case SyncStatusEnum.FAILED_TO_SAVE:
+                return (
+                    <SyncProblem color={iconColor} />
+                )
+            case SyncStatusEnum.IDLE:
+            default:
+                break;
+        }
+
+        return (
+            <CircularProgress size={16} />
+        );
+    }, [syncIconVerboseColor, verbose]);
+
+    const isIdle = syncStatus === SyncStatusEnum.IDLE;
+
+    if (isIdle) {
+        return <></>;
+    }
+
+    useEffect(() => {
+        // Whenever sync status changes, set verbose to true, then 3s later set it to false
+        setVerbose(true);
+        const timeout = setTimeout(() => {
+            setVerbose(false);
+        }, 1000);
+        return () => {
+            clearTimeout(timeout);
+        }
+    }, [syncStatus])
+
+    return (
+        <>
+            <Button
+                variant='text'
+                sx={(theme) => ({
+                    // border: '1px solid',
+                    // borderColor: 'rgba(0, 0, 0, 0.23)',
+                    color: theme.palette.text.secondary,
+                    // backgroundColor: theme.palette.action.hover,
+                    borderRadius: 16,
+                    py: 0.75,
+                    px: 1.5,
+                    // pr: 8
+                })}
+                startIcon={ButtonIcon}
+            >
+                <Collapse in={verbose} orientation="horizontal">
+                    <Typography variant="caption" sx={{ userSelect: 'none' }}>
+                        {syncStatusText}
+                        {/* <Shortcut>Ctrl</Shortcut> */}
+                    </Typography>
+                </Collapse>
+            </Button>
+        </>
+    )
+}
