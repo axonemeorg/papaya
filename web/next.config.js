6d6465ce61f95d14678eebdce28f2b64e11d99f6
Curtis Upshall <curtisupshall@gmail.com>
2024-12-01 13:32:11 -0800

    ZK-34: Created next.js rewrite for proxying to CouchDB

--- web/next.config.js
+++ web/next.config.js
@@ -3,8 +3,8 @@ const nextConfig = {
   rewrites: async () => {
     return [
       {
-        source: `${process.env.NEXT_PUBLIC_REMOTE_DB_PROXY_PATH}/:any*`,
-        destination: `${process.env.COUCHDB_URL}/:any`,
+        source: `${process.env.NEXT_PUBLIC_REMOTE_DB_PROXY_PATH}/:path*`,
+        destination: `${process.env.COUCHDB_URL}/:path*`,
       },
     ];
   },
