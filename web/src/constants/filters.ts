0bf9becc6de5bd5cc88ddcd51845b39462668a6c
Curtis Upshall <curtisupshall@gmail.com>
2024-12-07 14:38:53 -0800

    ZK-50: Refactor journal filter shorthand key map and add key serializer

--- web/src/constants/filters.ts
+++ web/src/constants/filters.ts
@@ -2,11 +2,11 @@ import { JournalAllFilters } from "@/components/journal/JournalFilters";
 
 type JournalFilterKeys = keyof JournalAllFilters; // Get the keys of JournalAllFilters
 
-type JournalFilterShorthandKeyMap = {
+type JournalFilterKeyMap = {
     [K in `${JournalFilterKeys}:`]: string;
 };
 
-export const JOURNAL_FILTER_SHORTHAND_KEY_MAP: JournalFilterShorthandKeyMap = {
+export const JOURNAL_FILTER_SHORTHAND_KEY_MAP: JournalFilterKeyMap = {
     'categories:': 'cs',
     'entryType:': 'et',
     'entryTags:': 'ts',
@@ -18,4 +18,15 @@ export const JOURNAL_FILTER_SHORTHAND_KEY_MAP: JournalFilterShorthandKeyMap = {
 
 export const JOURNAL_FILTER_SHORTHAND_KEY_MAP_INVERSE = Object.fromEntries(
     Object.entries(JOURNAL_FILTER_SHORTHAND_KEY_MAP).map(([key, value]) => [value, key])
-) as Record<string, keyof JournalFilterShorthandKeyMap>;
+) as Record<string, keyof JournalFilterKeyMap>;
+
+export const JOURNAL_FILTER_KEY_SERIALIZER = {
+    'categories:': (value: string[]) => value.join(','),
+    'entryType:': (value: string) => value,
+    'entryTags:': (value: string[]) => value.join(','),
+    'hasAttachments:': (value: boolean) => (value ? 'true' : 'false'),
+    'dateBefore:': (value: string) => value,
+    'dateAfter:': (value: string) => value,
+    'transactionAmountRange:': (value: [number, number]) => value.join(','),
+};
+
