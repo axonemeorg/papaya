f71d0e1ad0d0c9f684abd59cf21498d3839dacaa
Curtis Upshall <curtisupshall@gmail.com>
2024-12-13 00:07:57 -0800

    ZK-53: Added icon prop to CategoryChip component

--- web/src/components/icon/CategoryChip.tsx
+++ web/src/components/icon/CategoryChip.tsx
@@ -1,40 +1,44 @@
 import { Category } from '@/types/schema'
-import { alpha, Chip } from '@mui/material'
+import { alpha, Chip, Icon, useTheme } from '@mui/material'
+import AvatarIcon from './AvatarIcon'
 
 interface CategoryChipProps {
 	category?: Category
 	contrast?: boolean
+	icon?: boolean
 }
 
 export default function CategoryChip(props: CategoryChipProps) {
 	const categoryColor = props.category?.avatar.primaryColor
+	const theme = useTheme()
 
-	console.log(props)
+	let background = undefined;
+	let color = undefined;
+	if (categoryColor) {
+		if (props.contrast) {
+			background = categoryColor;
+			// use contrast color
+			color = theme.palette.getContrastText(categoryColor);
+		} else {
+			background = alpha(categoryColor, 0.125);
+			color = categoryColor;
+		}
+	}
 
 	return (
 		<Chip
-			size="small"
-			sx={(theme) => {
-				let background = undefined;
-				let color = undefined;
-				if (categoryColor) {
-					if (props.contrast) {
-						background = categoryColor;
-						// use contrast color
-						color = theme.palette.getContrastText(categoryColor);
-					} else {
-						background = alpha(categoryColor, 0.125);
-						color = categoryColor;
-					}
-				}
-				
-				return {
-					color,
-					background,
-					fontWeight: 500,
-				}
+			size={props.icon ? undefined : "small"}
+			sx={{				
+				color,
+				background,
+				fontWeight: 500,
 			}}
 			label={props.category?.label}
+			icon={props.icon && props.category ? (
+				<Icon>
+					<AvatarIcon avatar={props.category.avatar} sx={{ color: `${color} !important` }} />
+				</Icon>
+			) : undefined}
 		/>
 	)
 }
