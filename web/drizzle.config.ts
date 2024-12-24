d751851c5bf89037feb27c037cf1c210394d3cd5
Curtis Upshall <curtisupshall@gmail.com>
2024-12-24 14:58:59 -0800

    Renamed /database folder to /server and added README file.

--- web/drizzle.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'drizzle-kit';
-
-export default defineConfig({
-    dialect: 'postgresql',
-    schema: "./src/database/schemas/index.ts",
-    out: "./src/database/migrations",
-});
