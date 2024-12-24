98e3f1c15e0d3dfa8b4c95c32e3b1b8805246a38
Curtis Upshall <curtisupshall@gmail.com>
2024-12-24 11:43:47 -0800

    ZK-33: Use fuse to cache flattened Zisk journal documents for fuzzy searching

--- web/src/components/nav/header/search/SearchModal.tsx
+++ web/src/components/nav/header/search/SearchModal.tsx
@@ -11,21 +11,54 @@ import {
     Stack,
 } from "@mui/material";
 import { SearchLaunchButtonProps } from "./SearchLaunchButton";
-import { useRef, useState } from "react";
+import { useContext, useEffect, useMemo, useRef, useState } from "react";
 import { Cancel, Search } from "@mui/icons-material";
+import { JournalContext } from "@/contexts/JournalContext";
+import { getAllJournalObjects } from "@/database/actions";
+import Fuse from "fuse.js";
+import { flattenJournalObjects } from "@/utils/search";
 
 type SearchModalProps = DialogProps & Pick<SearchLaunchButtonProps, 'placeholderText'>
+
+const fuseOptions = {
+	keys: ['memo'], // Fields to search in
+	includeScore: true, // Include the score of how good each match is
+	threshold: 0.2, // Tolerance for fuzzy matching
+	minMatchCharLength: 2, // Minimum number of characters that must match
+}
 
 export default function SearchModal(props: SearchModalProps) {
     const { placeholderText, ...rest } = props
     const [query, setQuery] = useState<string>('')
     const inputRef = useRef<HTMLInputElement>(null)
+    const fuseRef = useRef<Fuse<any> | null>(null)
+    const journalContext = useContext(JournalContext)
     const hasError = false
 
     const handleClear = () => {
         setQuery('')
         inputRef.current?.focus()
     }
+
+    const results = useMemo(() => {
+        if (!query) {
+            return []
+        } else if (!fuseRef.current) {
+            return []
+        }
+
+        return fuseRef.current.search(query).map((result) => result.item)
+    }, [query])
+
+    useEffect(() => {
+        if (!journalContext.journal) {
+            return
+        }
+        getAllJournalObjects(journalContext.journal._id).then((objects) => {
+            const flattenedObjects = flattenJournalObjects(objects)
+            fuseRef.current = new Fuse(flattenedObjects, fuseOptions)
+        })
+    }, [props.open, journalContext.journal])
 
     return (
         <Dialog
@@ -80,7 +113,13 @@ export default function SearchModal(props: SearchModalProps) {
             </DialogContent>
             <Divider />
             <DialogContent>
-                
+                {results.map((result, index) => {
+                    return (
+                        <div key={index}>
+                            {JSON.stringify(result)}
+                        </div>
+                    )
+                })}
             </DialogContent>
         </Dialog>
     )
