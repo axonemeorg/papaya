c03cb481e5ca66dbfef7a6e962d4ec8a59d6b633
Curtis Upshall <curtisupshall@gmail.com>
2024-12-24 12:58:53 -0800

    ZK-33: Added full functionality to search modal

--- web/src/components/nav/header/search/SearchModal.tsx
+++ web/src/components/nav/header/search/SearchModal.tsx
@@ -8,28 +8,55 @@ import {
     IconButton,
     InputAdornment,
     InputBase,
+    ListItemIcon,
+    ListItemText,
+    MenuItem,
+    MenuList,
     Stack,
+    Typography,
 } from "@mui/material";
 import { SearchLaunchButtonProps } from "./SearchLaunchButton";
-import { useContext, useEffect, useMemo, useRef, useState } from "react";
+import { ReactNode, useCallback, useContext, useEffect, useRef, useState } from "react";
 import { Cancel, Search } from "@mui/icons-material";
 import { JournalContext } from "@/contexts/JournalContext";
 import { getAllJournalObjects } from "@/database/actions";
-import Fuse from "fuse.js";
+import Fuse, { FuseResult } from "fuse.js";
 import { flattenJournalObjects } from "@/utils/search";
+import { Category, ChildJournalEntry, JournalEntry, ZiskDocument } from "@/types/schema";
+import { calculateNetAmount, documentIsCategory, documentIsChildJournalEntry, documentIsJournalEntryOrChildJournalEntry } from "@/utils/journal";
+import { getPriceString } from "@/utils/string";
+import CategoryChip from "@/components/icon/CategoryChip";
+import AvatarIcon from "@/components/icon/AvatarIcon";
+import { PLACEHOLDER_UNNAMED_JOURNAL_ENTRY_MEMO } from "@/constants/journal";
+import { formatJournalEntryDate } from "@/utils/date";
+import { generateCategoryLink } from "@/utils/link";
+import Link from "next/link";
 
-type SearchModalProps = DialogProps & Pick<SearchLaunchButtonProps, 'placeholderText'>
+type SearchModalProps =
+    & DialogProps
+    & Pick<SearchLaunchButtonProps, 'placeholderText'>
+    & {
+        open: boolean
+        onClose: () => void
+    }
 
 const fuseOptions = {
-	keys: ['memo'], // Fields to search in
+	keys: ['memo', 'amount', 'label'], // Fields to search in
 	includeScore: true, // Include the score of how good each match is
 	threshold: 0.2, // Tolerance for fuzzy matching
 	minMatchCharLength: 2, // Minimum number of characters that must match
 }
 
+const Bullet = () => {
+    return (
+        <span>&bull;</span>
+    )
+}
+
 export default function SearchModal(props: SearchModalProps) {
     const { placeholderText, ...rest } = props
     const [query, setQuery] = useState<string>('')
+    const [results, setResults] = useState<FuseResult<ZiskDocument>[]>([])
     const inputRef = useRef<HTMLInputElement>(null)
     const fuseRef = useRef<Fuse<any> | null>(null)
     const journalContext = useContext(JournalContext)
@@ -40,15 +67,126 @@ export default function SearchModal(props: SearchModalProps) {
         inputRef.current?.focus()
     }
 
-    const results = useMemo(() => {
+    const renderResult = useCallback((result: FuseResult<ZiskDocument>, index: number) => {
+        const key = [result.item['_id'], index].filter(Boolean).join('-')
+    
+        let primaryTextFirstPart: ReactNode | undefined = undefined
+        let primaryTextSecondPart: ReactNode | undefined = undefined
+        let secondaryTextFirstPart: ReactNode | undefined = undefined
+        let secondaryTextSecondPart: ReactNode | undefined = undefined
+        let icon: ReactNode | undefined = undefined
+        let link: string | undefined = undefined
+        let onClickHandler: (() => void) | undefined = undefined
+    
+        // Journal Entry or Child Journal Entry
+        if (documentIsJournalEntryOrChildJournalEntry(result.item)) {
+            const netAmount = calculateNetAmount(result.item as JournalEntry)
+            const isNetPositive = netAmount > 0
+            const { categoryIds } = result.item
+            const categoryId: string | undefined = categoryIds?.[0]
+            const category: Category | undefined = categoryId
+                ? journalContext.getCategoriesQuery.data[categoryId]
+                : undefined
+    
+            primaryTextFirstPart = <span>{result.item.memo}</span>
+            primaryTextSecondPart = (
+                <Typography
+                    sx={(theme) => ({
+                        color: isNetPositive
+                            ? theme.palette.success.main
+                            : undefined,
+                    })}>
+                    {getPriceString(netAmount)}
+                </Typography>
+            )
+            secondaryTextFirstPart = formatJournalEntryDate(result.item.date)
+            icon = (
+                <AvatarIcon avatar={category?.avatar} />
+            )
+            onClickHandler = () => {
+                journalContext.editJournalEntry(result.item as JournalEntry)
+            }
+            
+        }
+
+        // Category
+        else if (documentIsCategory(result.item)) {
+            primaryTextFirstPart = <CategoryChip category={result.item} icon contrast />
+            link = generateCategoryLink(result.item)
+        }
+    
+        // Child Journal Entry
+        if (documentIsChildJournalEntry(result.item)) {
+            const parentJournalEntryMemo = result.item.parentEntry.memo ?? PLACEHOLDER_UNNAMED_JOURNAL_ENTRY_MEMO
+            secondaryTextSecondPart = <span>Belongs to <strong>{parentJournalEntryMemo}</strong></span>
+            onClickHandler = () => {
+                journalContext.editJournalEntry((result.item as ChildJournalEntry).parentEntry)
+            }
+        }
+
+        const MenuItemProps = link
+            ? { component: Link, href: link }
+            : {}
+    
+        return (
+            <MenuItem key={key} {...MenuItemProps} onClick={() => {
+                props.onClose?.()
+                if (onClickHandler) {
+                    onClickHandler()
+                }
+            }}>
+                {icon && (
+                    <ListItemIcon>
+                        {icon}
+                    </ListItemIcon>
+                )}
+                <ListItemText
+                    primaryTypographyProps={{
+                        sx: {
+                            display: 'flex',
+                            gap: 1,
+                        }
+                    }}
+                    primary={
+                        <>
+                            {primaryTextFirstPart}
+                            {primaryTextSecondPart && (
+                                <>
+                                    <Bullet />
+                                    {primaryTextSecondPart}
+                                </>
+                            )}
+                        </>
+                    }
+                    secondary={
+                        <>
+                            {secondaryTextFirstPart}
+                            {secondaryTextSecondPart && (
+                                <>
+                                    <Bullet />
+                                    {secondaryTextSecondPart}
+                                </>
+                            )}
+                        </>
+                    }
+                />
+            </MenuItem>
+        )
+    }, [journalContext.getCategoriesQuery.data])
+
+    const fetchResults = () => {
         if (!query) {
             return []
         } else if (!fuseRef.current) {
             return []
         }
 
-        return fuseRef.current.search(query).map((result) => result.item)
-    }, [query])
+        setResults(fuseRef.current.search(query))
+    }
+
+    useEffect(() => {
+        fetchResults()
+    }, [query, props.open])
 
     useEffect(() => {
         if (!journalContext.journal) {
@@ -59,6 +197,12 @@ export default function SearchModal(props: SearchModalProps) {
             fuseRef.current = new Fuse(flattenedObjects, fuseOptions)
         })
     }, [props.open, journalContext.journal])
+
+    useEffect(() => {
+        if (props.open) {
+            setQuery('')
+        }
+    }, [props.open])
 
     return (
         <Dialog
@@ -112,15 +256,17 @@ export default function SearchModal(props: SearchModalProps) {
                 </Stack>
             </DialogContent>
             <Divider />
-            <DialogContent>
-                {results.map((result, index) => {
-                    return (
-                        <div key={index}>
-                            {JSON.stringify(result)}
-                        </div>
-                    )
-                })}
-            </DialogContent>
+            {results.length === 0 ? (
+                <Typography variant='body2' sx={{ p: 2 }}>
+                    {query ? <>No results found for <i>{query}</i></> : 'Start typing to search...'}
+                </Typography>
+            ) : (
+                <MenuList>
+                    {results.map((result, index) => {
+                        return renderResult(result, index)
+                    })}
+                </MenuList>
+            )}
         </Dialog>
     )
 }
