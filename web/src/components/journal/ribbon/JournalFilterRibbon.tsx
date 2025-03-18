8358f90cbcc12323be4dbfa63cb19f457644dfa1
Curtis Upshall <curtisupshall@gmail.com>
2025-03-17 17:14:09 -0700

    ZK-112: Implements add filter button in filter ribbon component

--- web/src/components/journal/ribbon/JournalFilterRibbon.tsx
+++ web/src/components/journal/ribbon/JournalFilterRibbon.tsx
@@ -2,11 +2,16 @@ import AvatarChip from "@/components/icon/AvatarChip"
 import { JournalContext } from "@/contexts/JournalContext"
 import { JournalSliceContext } from "@/contexts/JournalSliceContext"
 import { Category } from "@/types/schema"
-import { Collapse, Stack } from "@mui/material"
-import { useContext } from "react"
+import { Add } from "@mui/icons-material"
+import { Collapse, IconButton, Stack } from "@mui/material"
+import { useContext, useRef, useState } from "react"
+import JournalFilterPicker from "./JournalFilterPicker"
 
 
 export default function JournalFilterRibbon() {
+    const [showFiltersMenu, setShowFiltersMenu] = useState<boolean>(false)
+    const filtersMenuButtonRef = useRef<HTMLButtonElement | null>(null)
+
     const journalSliceContext = useContext(JournalSliceContext)
 
     const numFilters = journalSliceContext.getSliceFilterCount()
@@ -28,21 +33,31 @@ export default function JournalFilterRibbon() {
     }
 
     return (
-        <Collapse in={numFilters > 0}>
-            <Stack direction='row' sx={{ flexFlow: 'row wrap', px: 2, py: 1 }} gap={0.5}>
-                {categoies.map((category) => {
-                    return (
-                        <AvatarChip
-                            key={category._id}
-                            avatar={category.avatar}
-                            label={category.label}
-                            icon
-                            // contrast
-                            onDelete={() => handleRemoveCategory(category._id)}
-                        />
-                    )
-                })}
-            </Stack>
-        </Collapse>
+        <>
+            <JournalFilterPicker
+                anchorEl={filtersMenuButtonRef.current}
+                open={showFiltersMenu}
+                onClose={() => setShowFiltersMenu(false)}
+            />
+            <Collapse in={numFilters > 0}>
+                <Stack direction='row' sx={{ flexFlow: 'row wrap', px: 2, py: 1 }} gap={0.5}>
+                    {categoies.map((category) => {
+                        return (
+                            <AvatarChip
+                                key={category._id}
+                                avatar={category.avatar}
+                                label={category.label}
+                                icon
+                                // contrast
+                                onDelete={() => handleRemoveCategory(category._id)}
+                            />
+                        )
+                    })}
+                    <IconButton size='small' ref={filtersMenuButtonRef} onClick={() => setShowFiltersMenu((showing) => !showing)}>
+                        <Add fontSize="small" sx={(theme) => ({ color: theme.palette.text.secondary })}/>
+                    </IconButton>
+                </Stack>
+            </Collapse>
+        </>
     )
 }
