64c10a5d3e8f1262da5a968dca82581b3044c478
Curtis Upshall <curtisupshall@gmail.com>
2025-03-22 00:43:13 -0700

    ZK-109: Create ZiskThemeProvider component

--- app/src/zisk/providers/index.tsx
+++ app/src/zisk/providers/index.tsx
@@ -1,15 +1,12 @@
 
-import { ThemeProvider } from '@mui/material'
 import { PropsWithChildren } from "react";
-import { CssBaseline } from '@mui/material'
-// import { montserrat } from '@/fonts/montserrat'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import NotificationsProvider from '@/providers/NotificationsProvider'
-import appTheme from '@/components/theme/theme'
 import JournalContextProvider from '@/providers/JournalContextProvider'
 import ZiskContextProvider from '@/providers/ZiskContextProvider'
 import RemoteContextProvider from '@/providers/RemoteContextProvider'
 import CalculatorContextProvider from '@/providers/CalculatorContextProvider'
+import ZiskThemeProvider from "./ZiskThemeProvider";
 
 interface ZiskProviders extends PropsWithChildren {
     queryClient: QueryClient
@@ -17,23 +14,20 @@ interface ZiskProviders extends PropsWithChildren {
 
 export default function ZiskProviders(props: ZiskProviders) {
     return (
-        <ThemeProvider theme={appTheme}>
-            <CssBaseline />
-            <main>
-                <NotificationsProvider>
-                    <QueryClientProvider client={props.queryClient}>
-                        <ZiskContextProvider>
-                            <RemoteContextProvider>
-                                <JournalContextProvider>
-                                    <CalculatorContextProvider>
-                                        {props.children}
-                                    </CalculatorContextProvider>
-                                </JournalContextProvider>
-                            </RemoteContextProvider>
-                        </ZiskContextProvider>
-                    </QueryClientProvider>
-                </NotificationsProvider>
-            </main>
-        </ThemeProvider>
+        <NotificationsProvider>
+            <QueryClientProvider client={props.queryClient}>
+                <ZiskContextProvider>
+                    <RemoteContextProvider>
+                        <JournalContextProvider>
+                            <ZiskThemeProvider>
+                                <CalculatorContextProvider>
+                                    {props.children}
+                                </CalculatorContextProvider>
+                            </ZiskThemeProvider>
+                        </JournalContextProvider>
+                    </RemoteContextProvider>
+                </ZiskContextProvider>
+            </QueryClientProvider>
+        </NotificationsProvider>
     )
 }
