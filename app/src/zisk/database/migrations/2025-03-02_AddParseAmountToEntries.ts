2692959461394076c9e6b90ffa0d8978655c7b9f
Curtis Upshall <curtisupshall@gmail.com>
2025-03-21 19:49:15 -0700

    ZK-120: move implementations of parseJournalEntryAmount and calculateNetAmount into AddParseAmountToEntries

--- app/src/zisk/database/migrations/2025-03-02_AddParseAmountToEntries.ts
+++ app/src/zisk/database/migrations/2025-03-02_AddParseAmountToEntries.ts
@@ -1,5 +1,4 @@
-import { JournalMeta, JournalVersion, ZiskDocument } from "@/types/schema";
-import { calculateNetAmount } from "@/utils/journal";
+import { JournalEntry, JournalMeta, JournalVersion, ZiskDocument } from "@/types/schema";
 import { Migration, MigrationRun } from "@/database/migrate";
 
 export default class AddParseAmountToEntries implements Migration {
@@ -13,12 +12,40 @@ export default class AddParseAmountToEntries implements Migration {
             if (record.type === 'JOURNAL_ENTRY') {
                 let parsedNetAmount: number | undefined = undefined
                 if (record.amount) {
-                    parsedNetAmount = calculateNetAmount(record)
+                    parsedNetAmount = AddParseAmountToEntries.calculateNetAmount(record)
                 }
                 record.parsedNetAmount = parsedNetAmount
             }
             acc.push(record)
             return acc
         }, []) as [JournalMeta, ...ZiskDocument[]]
+    }
+
+    private static parseJournalEntryAmount(amount: string): number | undefined {
+        const sanitizedAmount = String(amount).replace(/[^0-9.-]/g, '');
+        if (!amount || !sanitizedAmount) {
+            return undefined;
+        }
+    
+        const parsedAmount = parseFloat(sanitizedAmount)
+        if (isNaN(parsedAmount)) {
+            return parsedAmount
+        } else if (amount.startsWith('+')) {
+            return parsedAmount
+        } else {
+            return -parsedAmount
+        }
+    }
+
+    private static calculateNetAmount(entry: JournalEntry): number {
+        const children: JournalEntry[] = entry.children ?? []
+        const netAmount: number = children.reduce(
+            (acc: number, child) => {
+                return acc + (AddParseAmountToEntries.parseJournalEntryAmount(child.amount) ?? 0)
+            },
+            AddParseAmountToEntries.parseJournalEntryAmount(entry.amount) ?? 0
+        )
+    
+        return netAmount
     }
 }
