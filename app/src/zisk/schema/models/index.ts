9c0bd3345f51fc3b24e20313c3ff1c907bfe2f7d
Curtis Upshall <curtisupshall@gmail.com>
2025-04-15 20:34:55 -0700

    ZK-132: Refine types for ModelFactory

--- app/src/zisk/schema/models/index.ts
+++ app/src/zisk/schema/models/index.ts
@@ -1,4 +1,4 @@
-import { z, ZodSchema } from "zod";
+import { z, ZodRawShape, ZodSchema } from "zod";
 
 export const ModelBase = z.object({
     kind: z.string(),
@@ -17,27 +17,40 @@ export const ZiskModel = z.union([
 
 class ModelFactory {
     private static async create<M>(props: M): Promise<M> {
-        const result: M = await db.create(props)
-        return result
+      return db.create(props);
     }
-
-    public static extend<T extends ZodSchema>(schema: T) {
-        return class ExtendedModel {
-            public static async make(props: Partial<T>): Promise<T> {
-                return Promise.resolve<T>({
-                    ...props,
-                })
-            }
-
-            public static async create(props: Partial<T>): Promise<any> {
-                return ModelFactory.create<T>(await ExtendedModel.make(props));
-            }
+  
+    public static extend<Shape extends ZodRawShape>(shape: Shape) {
+      const schema = z.object(shape);
+      type SchemaType = z.infer<typeof schema>;
+  
+      return class ExtendedModel {
+        private static schema: typeof schema = schema;
+  
+        public static async make(props: Partial<SchemaType>): Promise<SchemaType> {
+          return schema.parse(props);
         }
+  
+        public static async create(props: Partial<SchemaType>): Promise<SchemaType> {
+          const parsed = await this.make(props);
+          return ModelFactory.create(parsed);
+        }
+  
+        public static validate(props: unknown): SchemaType {
+          return this.schema.parse(props);
+        }
+      };
+    }
+  }
+
+  class CategoryModel extends ModelFactory
+    .extend({
+        kind: z.literal('zisk:category'),
+        categoryId: z.string(),
+        label: z.string(),
+    }) {
+
+    static customLogic() {
+      // ...
     }
 }
-
-const CategoryModel = ModelFactory.extend({
-    kind: z.literal('zisk:category'),
-    categoryId: z.string(),
-    label: z.string(),
-});
