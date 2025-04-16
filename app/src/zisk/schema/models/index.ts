7fdf33007290d71f63f6859c2bc2798e9b9cfec7
Curtis Upshall <curtisupshall@gmail.com>
2025-04-15 21:20:44 -0700

    ZK-132: Introduces intrinsicSchema

--- app/src/zisk/schema/models/index.ts
+++ app/src/zisk/schema/models/index.ts
@@ -1,15 +1,15 @@
 import { z, ZodLiteral, ZodObject, ZodRawShape, ZodSchema } from "zod";
 
 export const ModelBase = z.object({
-    kind: z.string(),
-    _id: z.string(),
+  kind: z.string(),
+  _id: z.string(),
 })
 
 
 
 export const ZiskModel = z.union([
-    Category,
-    JournalEntry,
+  Category,
+  JournalEntry,
 ])
 
 
@@ -20,50 +20,68 @@ type ShapeWithKind = {
 } & ZodRawShape;
 
 class ModelFactory {
-    public static extend<Shape extends ShapeWithKind>(shape: Shape) {
-      const schema = z.object(shape);
-      type SchemaType = z.infer<typeof schema>;
-  
-      return class BaseModel {
-        public static schema: ZodObject<Shape> = schema;
-
-        public static async make(props: Partial<SchemaType>): Promise<SchemaType> {
-            return this.schema.parse(props);
-        }
-  
-        public static parse(props: unknown): SchemaType {
-          return this.schema.parse(props);
-        }
-      };
-    }
-  }
-  
-  // --- DOCUMENT FACTORY ---
-  
-  class DocumentFactory {
-    public static extend<Shape extends ShapeWithKind>(shape: Shape) {
-      const Base = ModelFactory.extend(shape);
-      type SchemaType = z.infer<typeof Base.schema>;
-  
-      return class DocumentModel extends Base {
-  
-        public static async create(props: Partial<SchemaType>): Promise<SchemaType> {
-          const parsed = await this.make(props);
-          return db.create(parsed);
-        }
-      };
-    }
+  private static intrinsicSchemaShape: ZodRawShape = {
+    _id: z.string(),
+    createdAt: z.string(),
   }
 
+  public static extend<Shape extends ShapeWithKind>(domainSchemaShape: Shape) {
+    const intrinsicSchema = z.object(ModelFactory.intrinsicSchemaShape)
+    const derivedSchema = intrinsicSchema.extend(domainSchemaShape);
 
-  class CategoryModel extends ModelFactory
-    .extend({
-        kind: z.literal('zisk:hello'),
-        categoryId: z.string(),
-        label: z.string(),
-    }) {
+    type DerivedSchemaType = z.infer<typeof derivedSchema>;
+    type IntrinsicSchemaType = z.infer<typeof intrinsicSchema>
 
-    static customLogic() {
-      // ...
-    }
+
+    return class BaseModel {
+      public static schema: ZodObject<DerivedSchemaType> = derivedSchema;
+
+      public static async intrinsics(): Promise<IntrinsicSchemaType> {
+        return {
+          _id: 'some-randomly-generated-id',
+          createdAt: new Date().toISOString(),
+        }
+      }
+
+      public static async make(props: Partial<DerivedSchemaType>): Promise<DerivedSchemaType> {
+        return {
+          ...(await this.intrinsics()),
+          ...props,
+        }
+      }
+
+      public static parse(props: unknown): DerivedSchemaType {
+        return this.schema.parse(props);
+      }
+    };
+  }
+}
+
+// --- DOCUMENT FACTORY ---
+
+class DocumentFactory {
+  public static extend<Shape extends ShapeWithKind>(domainSchemaShape: Shape) {
+    const Base = ModelFactory.extend(domainSchemaShape);
+
+    type SchemaType = z.infer<typeof Base.schema>;
+
+    return class DocumentModel extends Base {
+      public static async create(props: Partial<SchemaType>): Promise<SchemaType> {
+        const parsed = await this.make(props);
+        return db.create(parsed); // replace with real DB call
+      }
+    };
+  }
+}
+
+class CategoryModel extends ModelFactory
+  .extend({
+    kind: z.literal('zisk:category'),
+    categoryId: z.string(),
+    label: z.string(),
+  }) {
+
+  static customLogic() {
+    // ...
+  }
 }
