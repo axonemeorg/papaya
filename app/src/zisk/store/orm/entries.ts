c0e5b9991b8a9770be967a53e2f97c175af669c4
Curtis Upshall <curtisupshall@gmail.com>
2025-05-10 23:28:32 -0700

    ZK-132: Creates new state stores for Journal management modals

--- app/src/zisk/store/orm/entries.ts
+++
@@ -1,7 +0,0 @@
-import { JournalEntry } from "@/schema/documents/JournalEntry"
-import { createDocumentStore } from "../support/useDocumentStore"
-
-export const useJournalEntryStore = createDocumentStore<JournalEntry>()
-
-export const useJournalEntries = () => useJournalEntryStore((state) => state.items)
-export const useSetJournalEntries = () => useJournalEntryStore((state) => state.setItems)
