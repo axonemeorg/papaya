9a8a6fe93625cdd44607f21e4ad31b45289d283d
Curtis Upshall <curtisupshall@gmail.com>
2025-05-18 15:06:25 -0700

    ZK-132: Deletes orm Zustand stores (useCategories, etc)

--- app/src/zisk/store/orm/categories.ts
+++
@@ -1,9 +0,0 @@
-import { Category } from "@/schema/documents/Category"
-import { createDocumentStore } from "../support/useDocumentStore"
-
-export const useCategoryStore = createDocumentStore<Category>()
-
-export const useCategories = () => useCategoryStore((state) => state.items)
-export const useSetCategories = () => useCategoryStore((state) => state.setItems)
-export const useAddCategory = () => useCategoryStore((state) => state.addItem)
-export const useRemoveCategory = () => useCategoryStore((state) => state.deleteItem)
